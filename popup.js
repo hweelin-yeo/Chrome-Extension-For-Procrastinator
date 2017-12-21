e538b7db5fa719a0e0a8ae55a320d1aaefb37485
hweelin-yeo <yeohweelin_97@hotmail.com>
2017-12-21 01:28:33 -0500

    added popup.html

--- popup.js
+++ popup.js
@@ -1,1 +1,25 @@
-// Popup.js
+// Reference: The Chromium Authors. 
+
+'use strict';
+
+function setAlarm(event) {
+  let minutes = event.target.value;
+  chrome.pageAction.setBadgeText({text: 'ON'});
+  chrome.alarms.create({delayInMinutes: minutes});
+  chrome.storage.sync.set({minutes: minutes});
+  window.close();
+}
+
+function clearAlarm() {
+  chrome.browserAction.setBadgeText({text: 'OFF'});
+  chrome.alarms.clearAll();
+  window.close();
+}
+
+//An Alarm delay of less than the minimum 1 minute will fire
+// in approximately 1 minute incriments if released
+document.getElementById('Try').addEventListener('click', setAlarm);
+document.getElementById('5min').addEventListener('click', setAlarm);
+document.getElementById('10min').addEventListener('click', setAlarm);
+document.getElementById('15min').addEventListener('click', setAlarm);
+document.getElementById('cancelAlarm').addEventListener('click', clearAlarm);
