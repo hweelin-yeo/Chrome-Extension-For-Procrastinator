5cbf5d6f87f6e0bd1ad33e258592cdc4933b83f9
hweelin-yeo <hy384@cornell.edu>
2017-12-28 18:12:06 -0800

    fixed badge text

--- background.js
+++ background.js
@@ -2,134 +2,59 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback called when the URL of the current tab
- *   is found.
- */
 
+function setAlarm() {
+  console.log("setAlarm Function called")
+	chrome.storage.sync.get(['minutes'], function(item) {
+      chrome.browserAction.setBadgeText({text: 'ON'});
+      chrome.alarms.create({periodInMinutes: item.minutes});
+    });
+  };
 
-function playVideo() {
-  var video = document.createElement("video");
-  video.setAttribute("src", ".mp4");
+function noAlarmFB() {
+  chrome.browserAction.setBadgeBackgroundColor({color: "#8B4513"});
+  chrome.browserAction.setBadgeText({text: 'FB'});
 }
 
+function noAlarmNoFB() {
+  chrome.browserAction.setBadgeBackgroundColor({color: "#8B4513"});
+  chrome.browserAction.setBadgeText({text: ''});
+}
+
+function configureAlarm() {
+  chrome.storage.sync.get(['onAlarm'], function(item) {
+      chrome.alarms.getAll(function (alarms) {
+        if (item.onAlarm == true && alarms.length == 0) {
+          setAlarm();
+        } else if (item.onAlarm == null || item.onAlarm == false) {
+          console.log("uhm");
+          noAlarmFB();
+        }
+      });
+    });
+}
+
+function checkForValidUrl(tabId, changeInfo, tab) {
+  if (tab.url.indexOf("facebook") == -1) {
+    noAlarmNoFB();
+  } else {
+    configureAlarm();
+  };
+};
+
+function playVideo() {
+  // chrome.tabs.executeScript(null, {file: 'content.js'});
+  chrome.notifications.create({
+      type:     'basic',
+      iconUrl:  'angryman.png',
+      title:    'Do work!!',
+      message:  'Go back to work!',
+      buttons: [
+        {title: 'Keep working'}
+      ],
+      priority: 0});
+};
+
+// Listen for any changes to the URL of any tab.
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
 chrome.alarms.onAlarm.addListener(playVideo);
-
-// function getCurrentTabUrl(callback) {
-//   // Query filter to be passed to chrome.tabs.query - see
-//   // https://developer.chrome.com/extensions/tabs#method-query
-//   var queryInfo = {
-//     active: true,
-//     currentWindow: true
-//   };
-
-//   chrome.tabs.query(queryInfo, (tabs) => {
-//     // chrome.tabs.query invokes the callback with a list of tabs that match the
-//     // query. When the popup is opened, there is certainly a window and at least
-//     // one tab, so we can safely assume that |tabs| is a non-empty array.
-//     // A window can only have one active tab at a time, so the array consists of
-//     // exactly one tab.
-//     var tab = tabs[0];
-
-//     // A tab is a plain object that provides information about the tab.
-//     // See https://developer.chrome.com/extensions/tabs#type-Tab
-//     var url = tab.url;
-
-//     // tab.url is only available if the "activeTab" permission is declared.
-//     // If you want to see the URL of other tabs (e.g. after removing active:true
-//     // from |queryInfo|), then the "tabs" permission is required to see their
-//     // "url" properties.
-//     console.assert(typeof url == 'string', 'tab.url should be a string');
-
-//     callback(url);
-//   });
-
-//   // Most methods of the Chrome extension APIs are asynchronous. This means that
-//   // you CANNOT do something like this:
-//   //
-//   // var url;
-//   // chrome.tabs.query(queryInfo, (tabs) => {
-//   //   url = tabs[0].url;
-//   // });
-//   // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-// }
-
-// /**
-//  * Change the background color of the current page.
-//  *
-//  * @param {string} color The new background color.
-//  */
-// function changeBackgroundColor(color) {
-//   var script = 'document.body.style.backgroundColor="' + color + '";';
-//   // See https://developer.chrome.com/extensions/tabs#method-executeScript.
-//   // chrome.tabs.executeScript allows us to programmatically inject JavaScript
-//   // into a page. Since we omit the optional first argument "tabId", the script
-//   // is inserted into the active tab of the current window, which serves as the
-//   // default.
-//   chrome.tabs.executeScript({
-//     code: script
-//   });
-// }
-
-// *
-//  * Gets the saved background color for url.
-//  *
-//  * @param {string} url URL whose background color is to be retrieved.
-//  * @param {function(string)} callback called with the saved background color for
-//  *     the given url on success, or a falsy value if no color is retrieved.
- 
-// function getSavedBackgroundColor(url, callback) {
-//   // See https://developer.chrome.com/apps/storage#type-StorageArea. We check
-//   // for chrome.runtime.lastError to ensure correctness even when the API call
-//   // fails.
-//   chrome.storage.sync.get(url, (items) => {
-//     callback(chrome.runtime.lastError ? null : items[url]);
-//   });
-// }
-
-// /**
-//  * Sets the given background color for url.
-//  *
-//  * @param {string} url URL for which background color is to be saved.
-//  * @param {string} color The background color to be saved.
-//  */
-// function saveBackgroundColor(url, color) {
-//   var items = {};
-//   items[url] = color;
-//   // See https://developer.chrome.com/apps/storage#type-StorageArea. We omit the
-//   // optional callback since we don't need to perform any action once the
-//   // background color is saved.
-//   chrome.storage.sync.set(items);
-// }
-
-// // This extension loads the saved background color for the current tab if one
-// // exists. The user can select a new background color from the dropdown for the
-// // current page, and it will be saved as part of the extension's isolated
-// // storage. The chrome.storage API is used for this purpose. This is different
-// // from the window.localStorage API, which is synchronous and stores data bound
-// // to a document's origin. Also, using chrome.storage.sync instead of
-// // chrome.storage.local allows the extension data to be synced across multiple
-// // user devices.
-// document.addEventListener('DOMContentLoaded', () => {
-//   getCurrentTabUrl((url) => {
-//     var dropdown = document.getElementById('dropdown');
-
-//     // Load the saved background color for this page and modify the dropdown
-//     // value, if needed.
-//     getSavedBackgroundColor(url, (savedColor) => {
-//       if (savedColor) {
-//         changeBackgroundColor(savedColor);
-//         dropdown.value = savedColor;
-//       }
-//     });
-
-//     // Ensure the background color is changed and saved when the dropdown
-//     // selection changes.
-//     dropdown.addEventListener('change', () => {
-//       changeBackgroundColor(dropdown.value);
-//       saveBackgroundColor(url, dropdown.value);
-//     });
-//   });
-// });
